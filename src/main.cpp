8e691d5ef9008b6bb1c45e825ab910b84463de31
BAKAJ77 <44305796+BAKAJ77@users.noreply.github.com>
2024-08-02 01:37:37 +0100

    Added support for specifying CHIP-8 program file path via argv

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,33 @@
 #include <core/interpreter.h>
+#include <debugging.h>
 #include <iostream>
 
 int main(int argc, char** argv)
 {
     try
     {
+        // Get the specified file path of the CHIP-8 program
+        if (argc < 2)
+            throw std::runtime_error("No CHIP-8 program file was specified\n");
+
+        const std::string filePath = argv[1];
+
+        // Initialize the emulator window and renderer
+        OutputLog("[Info] Initializing emulator window\n");
         WindowFrame emulatorWindow("Chip-8 Emulator");
+
+        OutputLog("[Info] Initializing emulator renderer\n");
         GraphicsRenderer& renderer = emulatorWindow.GetRenderer();
         
+        // Initialize the emulator interpreter and load the CHIP-8 program
+        OutputLog("[Info] Initializing emulator interpreter\n");
         EmulatorInterpreter interpreter;
-        interpreter.LoadProgram("tetris.c8");
 
-        while (!interpreter.ShouldTerminate()) // The emulator game loop
+        OutputLog("[Info] Loading the CHIP-8 program: %s\n", filePath.c_str());
+        interpreter.LoadProgram(filePath);
+
+        // The emulator game loop
+        while (!interpreter.ShouldTerminate())
         {
             interpreter.Update(emulatorWindow);
             interpreter.Render(renderer);
@@ -19,7 +35,12 @@ int main(int argc, char** argv)
     }
     catch (const std::exception& e)
     {
-        std::cerr << e.what() << '\n';
+        OutputLog("[Error] %s\n", e.what());
+
+#ifdef DEBUG_MODE
+        std::cin.get(); // Pause termination so that the debugging console can be read by the user before exiting
+#endif
+
         return EXIT_FAILURE;
     }
 
